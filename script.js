57c0908a10e8fb9cd6e60a5a37889a563acb8b7f
brownied <84053724+brownied@users.noreply.github.com>
2025-12-27 18:41:20 -0600

    create get computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello, world!");
+function getComputerChoice(){
+    let randInt = getRandomInt(3);
+    switch(randInt){
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break
+    }
+}
+
+function getRandomInt(max){
+    return Math.floor(Math.random() * max);
+}
+
